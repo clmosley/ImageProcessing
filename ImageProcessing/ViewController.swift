b80a07759e3dc4e37e469f9aa48bf537e32da7a7
Connor Mosley <connor.l.mosley@gmail.com>
2016-08-23 00:09:19 -0700

    Initial Commit. App accesses the forward facing camera and displays the image on the screen.

--- ImageProcessing/ViewController.swift
+++ ImageProcessing/ViewController.swift
@@ -7,12 +7,50 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    let captureSession = AVCaptureSession()
+    
+    // If we find a device we'll store it here for later use
+    var captureDevice : AVCaptureDevice?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        captureSession.sessionPreset = AVCaptureSessionPresetLow
+        let devices = AVCaptureDevice.devices()
+        print(devices)
+        
+        // Loop through all the capture devices on this phone
+        for device in devices {
+            // Make sure this particular device supports video
+            if (device.hasMediaType(AVMediaTypeVideo)) {
+                // Finally check the position and confirm we've got the back camera
+                if(device.position == AVCaptureDevicePosition.Back) {
+                    captureDevice = device as? AVCaptureDevice
+                }
+            }
+        }
+        if captureDevice != nil {
+            beginSession()
+        }
+    }
+    
+    func beginSession() {
+//        var err : NSError? = nil
+        do {
+            try self.captureSession.addInput(AVCaptureDeviceInput(device: self.captureDevice))
+        } catch {
+//            print("error: \(err)")
+            print("There was an error")
+        }
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        self.view.layer.addSublayer(previewLayer)
+        previewLayer?.frame = self.view.layer.frame
+        captureSession.startRunning()
     }
 
     override func didReceiveMemoryWarning() {
